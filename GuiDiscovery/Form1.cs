06441301f50a1f855be510dce9fdcd7d10d39d90
Jeremy Bunce <jerbunce@Uat.edu>
2023-07-25 23:56:37 -0700

    Add comments

--- GuiDiscovery/Form1.cs
+++ GuiDiscovery/Form1.cs
@@ -15,19 +15,24 @@ namespace GuiDiscovery
         private void button1_Click(object sender, EventArgs e)
         {
             {
-
+                // Reads each to the words in the word list
                 IEnumerable<string> lines = File.ReadLines(fileName);
+
+                // Run for each word in word list
                 foreach (var lineRead in lines)
                 {
+                    
+                    // adds user input with a slash and the current word in the wordlist
                     string request = textBox_url.Text + "/" + lineRead;
+                    
                     try
                     {
-
+                        // Make web request and get a response 
                         HttpWebRequest myHttpWebRequest = (HttpWebRequest)WebRequest.Create(request);
 
                         HttpWebResponse myHttpWebResponse = (HttpWebResponse)myHttpWebRequest.GetResponse();
 
-
+                        // if the request is sucessful add to the list box
                         if (myHttpWebResponse.StatusCode == HttpStatusCode.OK)
 
                             listBox_url.Items.Add(request);
@@ -51,7 +56,7 @@ namespace GuiDiscovery
 
         private void button_wl_Click(object sender, EventArgs e)
         {
-
+            // if the file path user picks is possible then make that the new word list path
             if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
                 fileName = openFileDialog.FileName;
